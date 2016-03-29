905ef2fcf9031cdf329a6527a72001dae7fcf59f
AnnaOzer <ozerskaya.ru@mail.ru>
2016-03-29 16:53:07 +0400

    + обработка данных из $_POST в контроллере добавления новостей

--- controllers/add_news.php
+++ controllers/add_news.php
@@ -1,3 +1,17 @@
 <?php
 
+unset($article);
+
+if(!empty($_POST)) {
+    if(isset($_POST['text'])){
+        $article = [];
+        $article['text'] = $_POST['text'];
+        $article['title'] = isset($_POST['title']) ? $_POST['title'] : 'Без названия';
+        }
+}
+
+if(isset($article)) {
+
+}
+
 header('Location: ../index.php');
