f070c2bf10b3b856423a991aed02f9bcdfaf0701
AnnaOzer <ozerskaya.ru@mail.ru>
2016-03-30 13:52:46 +0400

    + к странице изменения статьи подключена форма, по id виден текст

--- view/form_change.php
+++ view/form_change.php
@@ -1,5 +1,5 @@
 <form method="post" action="../controllers/change_news.php">
-    Заголовок статьи: <input type="text" name="title"><br><br>
-    Текст новости: <textarea name="text"></textarea><br>
+    Заголовок статьи: <input type="text" name="title" value="<?php echo $article['title']; ?>"><br><br>
+    Текст новости: <textarea name="text" ><?php echo $article['text']; ?></textarea><br>
     <input type="submit">
 </form>
