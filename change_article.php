636034692caf4921a89912e4265477566e6fd231
AnnaOzer <ozerskaya.ru@mail.ru>
2016-03-31 12:49:59 +0400

    + вью 404, показывается при попытке изменить новость которой нет

--- change_article.php
+++ change_article.php
@@ -8,7 +8,12 @@ $id = !empty($_GET['id']) ? $_GET['id'] : 1;
 // обратиться к модели за одной новостью
 $article = News_getById($id);
 
-// показать статью, в экшен перенаправить на обработчик
-include 'view/form_change.php';
+// если в $article null, показать 404
+if(null === $article && isset($_GET['id'])) {
+    include __DIR__ . '/view/404.php';
+} else {
+// если в $article не null показать статью, в экшен перенаправить на обработчик
+    include 'view/form_change.php';
+}
 
 
