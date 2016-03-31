497a4764a61a7c3cc9498ae2c1c13f20e39c70c8
AnnaOzer <ozerskaya.ru@mail.ru>
2016-03-31 12:52:46 +0400

    + вью 404 показывается при попытке _показать_ новость,  которой нет

--- one.php
+++ one.php
@@ -8,5 +8,10 @@ $id = !empty($_GET['id']) ? $_GET['id'] : 1;
 // обратиться к модели за одной новостью
 $article = News_getById($id);
 
+// если в $article null, показать 404
+if(null === $article && isset($_GET['id'])) {
+    include __DIR__ . '/view/404.php';
+} else {
 // вывести эту одну новость
-include 'view/one_article.php';
+    include 'view/one_article.php';
+}
