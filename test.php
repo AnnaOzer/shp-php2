ff211d9358bf1690e217d14eb296526b0e2796f7
AnnaOzer <ozerskaya.ru@mail.ru>
2016-04-06 16:53:49 +0400

    + создан еще один объект класса, у него другие свойства

--- test.php
+++ test.php
@@ -9,10 +9,14 @@ class Article {
 }
 
 $art = new Article;
-$art->{title} = "Супер новость!";
+$art->title = "Супер новость!";
 $art->text = "Текст новости";
 
 $art->title = "Не очень супер";
+
+$art1 = new Article;
+$art1->title = '1';
+$art1->text = '11';
 ?>
 
 <article>
